ff37ffb26d1aa3f0a435c95c543976179e026980
umeshkafle <u.kafle@hotmail.com>
2017-02-07 09:26:33 +0545

    A controller called Subjects is created and also a gem called Devise is added for authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
  
 
+  get 'subjects/index'
+
+  get 'subjects/show'
+
+  get 'subjects/new'
+
+  get 'subjects/create'
+
+  get 'subjects/edit'
+
+  get 'subjects/update'
+
+  get 'subjects/destroy'
+
  root "dashboard#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
